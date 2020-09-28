7bced9446ecdcf67eac0983567c7defdc84ea5da
Дима Шитов <dimahp2013@yandex.ru>
2020-09-28 20:43:35 +0300

    Добавьте файлы проекта.

---
+++ HulkApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HulkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter number from 1 to 100");
+            int n = Convert.ToInt32(Console.ReadLine());
+           
+            string result;
+
+            result = string.Join(" that ", Enumerable.Range(1, n).Select(i => $"i {(i % 2 == 1 ? "hate" : "love")}")) + " it";
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
